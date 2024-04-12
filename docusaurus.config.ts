f76719123c80ce143283455c3d931779e9038f6e
ramirezfernando <ramirez.fernando2003@gmail.com>
2024-04-12 01:40:15 -0500

    feat: add first post and update favicon

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,7 +5,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 const config: Config = {
   title: 'Blog',
   tagline: 'Dinosaurs are cool',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/gopher.png',
 
   // Set the production url of your site here
   url: 'https://your-docusaurus-site.example.com',
